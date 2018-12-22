984c1676fbd7bd264a1165ccc625fdad7df15fca
bluewind <zero18030@gmail.com>
2018-12-22 20:31:11 +0900

    Enumの修正 設定の参照ミスがあったための、作り直し

--- app/src/main/java/momonyan/weathergeta/WeatherEnum.kt
+++ app/src/main/java/momonyan/weathergeta/WeatherEnum.kt
@@ -1,14 +1,11 @@
 package momonyan.weathergeta
 
 public enum class WeatherEnum(val id: Int, val eng: String, val kanzi: String, val hiragana: String) {
-    ClearSky(0, "Clear sky", "快晴", "かいせい"),
-    FewClouds(1, "Few clouds", "晴れ", "はれ"),
-    ScatteredClouds(2, "Scattered clouds", "曇り", "くもり"),
-    BrokenClouds(3, "Broken clouds", "ちぎれ雲", "ちぎれぐも"),
-    ShowerRain(4, "Shower rain", "にわか雨", "にわかあめ"),
-    Rain(5, "Rain", "雨", "あめ"),
-    ThunderStorm(6, "Thunderstorm", "雷", "かみなり"),
-    Snow(7, "Snow", "雪", "ゆき"),
-    Mist(8, "Mist", "霧", "きり")
+    Clear(0, "Clear", "快晴", "かいせい"),
+    Clouds(1, "Clouds", "曇り", "くもり"),
+    Rain(2, "Rain", "雨", "あめ"),
+    ThunderStorm(3, "Thunderstorm", "雷", "かみなり"),
+    Snow(4, "Snow", "雪", "ゆき"),
+    Mist(5, "Fog", "霧", "きり")
 
 }
